48c552198878d5576fc724426820f08605bf102a
Sabeur Thabti <thabti.sabeur@gmail.com>
2017-02-11 11:10:24 +0000

    add smart component

--- src/container/index.js
+++ src/container/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import style from'./style.scss';
-console.log(style);
-const App = () => (
-	<div className={style.main}>
-		<h2>Patterns Lib</h2>
-	</div>
-);
+import style from './style.scss';
+
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+	render() {
+		return (
+			<div className={style.main}>
+				<h2>Hello World</h2>
+			</div>
+		);
+	}
+}
 
 export default App;
